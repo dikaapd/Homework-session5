81e04dcea35e6dd00a923c9e85e7e25a8e9757d6
dikaapd <dikaprasetyad77.gmail.com>
2026-08-11 11:10:11 +0700

    commiting homework for session 3

---
+++ homework_session3/homework-session3-loopstatement.js
@@ -0,0 +1,28 @@
+// for loop statement
+let pola ="";
+
+for (let i = 1; i <= 4; i++) {
+   pola = pola + "*";
+   console.log(pola);
+}
+// -----------------------------------
+// while loop statement
+// let pola ="";
+// let i = 1;
+
+// while (i <=4) {
+//     pola = pola + "*";
+//     console.log(pola);
+//     i++;
+// }
+// ------------------------------------
+// do while statement
+// let pola = "";
+// let i = 1;
+
+// do {
+//     pola = pola + "*";
+//     console.log(pola);
+//     i++;
+// } while (i<=4);
+// -------------------------------------
